49c01293e51c3a40b48d4b5c77c0158b7a8f69d1
Ben Jones <18bjones@thelangton.org.uk>
2024-01-15 10:40:17 +0000

    Added row logic

---
+++ board.gd
@@ -0,0 +1,12 @@
+extends Control
+
+# Script that controls the master logic for the game, such as generating the code 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ row.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+func row_lock():
+	#a function to stop the editing of a row after it has been played
+	pass
+
+func check_row():
+	# to be called when the check button is pressed
+	# Needs to check and return the value of the code
+	# so that the button can display the answers
+	pass
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#if 
+	#	check()
+	pass
